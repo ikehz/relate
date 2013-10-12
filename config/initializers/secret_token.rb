ce43474040d1d73e7ab58577de782d5ab3aa865d
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-10-12 16:14:37 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leucurus::Application.config.secret_key_base = '4584effac2245aafb9a34550ba246e3cf1d35b804bc93f6ae10ea6d820def6c997d7ec1b1d16cb32b93db1303964a5e5eac31b451d30591b154a202e595a1dbd'
