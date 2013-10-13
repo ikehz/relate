6f47830159d1579bc65b509e440659b8c392de44
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-10-13 14:53:39 -0400

    basic home#index tests done

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,27 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+
+  context "an anonymous user" do
+
+    should "get index" do
+      get :index
+      assert_response :success
+    end
+
+  end
+
+  context "a signed-in user" do
+
+    setup do
+      sign_in @u = User.make!
+    end
+
+    should "get index" do
+      get :index
+      assert_response :success
+    end
+
   end
 
 end
