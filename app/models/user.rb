de59810528f3c91db28364ad90b7d8d90d86ea01
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-10-16 11:54:45 -0400

    added grants to users, but just allowed everything; relying on Devise for user security

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
 	       :confirmable, :lockable, :timeoutable, :omniauthable
 
+  # rely in Devise for user security
+  #
+  # TODO is this really a good idea?
+  #
+  # We probably need to modify Grant to work with column-level security so that
+  # we can modify certain things as anonymous and not others
+  grant(:find) { true }
+  grant(:create, :update, :destroy) { true }
+  # grant(:create, :update, :destroy) { |user, model, action| user == model }
+
   validates :username, presence: true, format: { with: /\A[0-9a-z_\-]+\z/ }, uniqueness: true
   validates :name, format: { with: /\A[A-Za-z ]*\z/ }
 end
