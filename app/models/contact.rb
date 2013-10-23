88a083abda8ef209698fc4b1ab168acbae3f3db8
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-10-23 11:35:50 -0400

    changed contact and conversation relationships to participations dependent: :destroy

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
-  has_many :participations, dependent: :restrict_with_exception
+  has_many :participations, dependent: :destroy
   has_many :conversations, through: :participations
 
   grant(:find, :create, :update, :destroy) { |user, model, action| model.user == user }
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ActiveRecord::Base
   belongs_to :user
-  has_many :participations, dependent: :restrict_with_exception
+  has_many :participations, dependent: :destroy
   has_many :contacts, through: :participations
 
   grant(:find, :create, :update, :destroy) { |user, model, action| model.user == user }
