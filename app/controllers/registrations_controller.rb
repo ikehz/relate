84350d7664bac8a9af9e53eeda266e5658d7b7ae
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-11-05 10:47:20 -0500

    added username to devise views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
 
+  # XXX should this be private or protected?
   protected
- 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).concat([:username,:name])
-    devise_parameter_sanitizer.for(:sign_in).concat([:username,:name])
-    devise_parameter_sanitizer.for(:account_update).concat([:username,:name])
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up).concat([:username,:name])
+      devise_parameter_sanitizer.for(:sign_in).concat([:username,:name])
+      devise_parameter_sanitizer.for(:account_update).concat([:username,:name])
+    end
 end
