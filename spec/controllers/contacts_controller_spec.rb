07df3fd3ed7cd84c8cf47246194335386919b5a4
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-11-26 23:14:12 -0500

    refactored contacts_controller_spec

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe ContactsController do
-  it_behaves_like "an owned resource controller" do
-    let (:resource) { :contact }
-    let (:updatable_attribute) { :name }
+  let (:user) { User.make! }
+
+  context "when a user is signed-in" do
+    before do
+      sign_in user
+    end
+
+    it_behaves_like "a RESTful resource" do
+      let (:resource_name) { :contact }
+      let (:resource) { Contact.make!(owner: user) }
+      let (:invalid_resource) { Contact.new }
+      let (:update_resource) { Contact.make(owner: user) }
+      let (:update_attribute) { :name }
+    end
   end
+
 end
