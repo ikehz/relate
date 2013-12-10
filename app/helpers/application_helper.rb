12a948b35485c56ab780077d8f9543ee08a5db7f
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-12-10 11:19:40 -0500

    fixed flash messages to only display specific types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,13 @@
 module ApplicationHelper
   TRUNCATE_LENGTH = 90.freeze
 
-  def bootstrap_class_for flash_type
-    case flash_type
-    when :success
-      "alert-success"
-    when :error
-      "alert-error"
-    when :alert
-      "alert-block"
-    when :notice
-      "alert-info"
-    else
-      flash_type.to_s
-    end
-  end
+  BOOTSTRAP_FLASH_TYPES = {success: 'alert-success', error: 'alert-error', alert: 'alert-block', notice: 'alert-info'}.freeze
 
   def truncate_length
     TRUNCATE_LENGTH
+  end
+
+  def bootstrap_flash_types
+    BOOTSTRAP_FLASH_TYPES
   end
 end
