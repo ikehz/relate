1998685850435f3419dd438c3850d08324b5d5df
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-12-13 11:57:16 -0500

    added contacts_spec draft

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -1,11 +1,69 @@
 require 'spec_helper'
 
-describe "Contacts", type: :feature do
-  context "GET #index"
-  context "GET #new"
-  context "POST #create"
-  context "GET #show"
-  context "GET #edit"
-  context "PATCH #update"
-  context "DELETE #destroy"
+describe "Contacts" do
+  let (:user) { User.make! }
+
+  before do
+    @contact = Contact.make!(owner: user)
+    @new_contact = Contact.make(owner: user)
+  end
+
+  context "a user" do
+    before do
+      sign_in user
+    end
+
+    it "gets the index" do
+      visit contacts_url
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content(@contact.name)
+    end
+
+    it "creates a new contact" do
+      visit contacts_url
+
+      expect{
+        click_link 'New Contact'
+        fill_in 'Name', with: @new_contact.name
+        click_button 'Create Contact'
+      }.to change(Contact, :count).by 1
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content('Contact was successfully created.')
+      expect(page).to have_content(@new_contact.name)
+    end
+
+    it "shows a contact" do
+      visit contact_url(@contact)
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content(@contact.name)
+    end
+
+    it "edits a contact" do
+      visit contact_url(@contact)
+
+      click_link 'Edit Contact'
+      fill_in 'Name', with: @new_contact.name
+      click_button 'Update Contact'
+
+      expect(@contact.reload.name).to eq(@new_contact.name)
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content('Contact was successfully updated.')
+      expect(page).to have_content(@new_contact.name)
+    end
+
+    it "destroys a contact" do
+      visit contacts_url
+
+      expect{
+        click_link 'Destroy'
+      }.to change(Contact, :count).by -1
+
+      expect(page.status_code).to eq(200)
+      expect(page).to_not have_content(@contact.name)
+    end
+  end
 end
