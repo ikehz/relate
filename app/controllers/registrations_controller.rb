f2864dc2f31835e548669674adc96d9aaff5e0ea
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-12-30 14:31:52 -0500

    use username instead of email for logging in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
     # Never trust parameters from the scary internet, only allow the white list through.
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up).concat([:username,:name])
-      devise_parameter_sanitizer.for(:sign_in).concat([:username,:name])
+      devise_parameter_sanitizer.for(:sign_in).concat([:username])
       devise_parameter_sanitizer.for(:account_update).concat([:username,:name])
     end
 end
--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -8,7 +8,7 @@ describe "Sessions" do
       visit root_path
 
       click_link 'Sign in'
-      fill_in 'Email', with: user.email
+      fill_in 'Username', with: user.username
       fill_in 'Password', with: user.password
       click_button 'Sign in'
 
