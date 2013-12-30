5bb72c2900e451f5c3ae88b7d99725548df0e31a
Isaac Hollander McCreery <ihmccreery@gmail.com>
2013-12-30 17:34:38 -0500

    more tests for registrations

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -1,22 +1,71 @@
 require 'spec_helper'
 
 describe "Registrations" do
-  let (:user) { User.make }
+  before do
+    @user = User.make!
+    @new_user = User.make
+  end
 
   context "a user" do
     it "registers" do
       visit root_path
 
       click_link 'Sign up'
-      fill_in 'Username', with: user.username
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password, match: :prefer_exact
-      fill_in 'Password confirmation', with: user.password, match: :prefer_exact
+      fill_in 'Username', with: @new_user.username
+      fill_in 'Email', with: @new_user.email
+      fill_in 'Password', with: @new_user.password, match: :prefer_exact
+      fill_in 'Password confirmation', with: @new_user.password, match: :prefer_exact
       click_button 'Sign up'
 
       expect(page.status_code).to eq(200)
       expect(current_path).to eq(root_path)
       expect(page).to have_content('A message with a confirmation link has been sent to your email address.')
+    end
+
+    it "updates registration" do
+      sign_in @user
+
+      visit root_path
+
+      click_link 'Edit profile'
+      fill_in 'Username', with: @new_user.username
+      fill_in 'Current password', with: @user.password, match: :prefer_exact
+      click_button 'Update'
+
+      expect(@user.reload.username).to eq(@new_user.username)
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('You updated your account successfully.')
+    end
+
+    it "updates password" do
+      sign_in @user
+
+      visit root_path
+
+      click_link 'Edit profile'
+      fill_in 'Password', with: @new_user.password, match: :prefer_exact
+      fill_in 'Password confirmation', with: @new_user.password, match: :prefer_exact
+      fill_in 'Current password', with: @user.password, match: :prefer_exact
+      click_button 'Update'
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('You updated your account successfully.')
+    end
+
+    it "cancels registration" do
+      sign_in @user
+
+      visit root_path
+
+      click_link 'Edit profile'
+      click_link 'Cancel my account'
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('Your account was successfully cancelled.')
     end
   end
 end
