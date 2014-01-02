1edfde81609d855d31b30b52ce07d1649b3a9530
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-01-02 17:14:03 -0500

    changed mailers to use username and remind of username in addition to password

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -22,6 +22,39 @@ describe "Registrations" do
       expect(page).to have_content('A message with a confirmation link has been sent to your email address.')
     end
 
+    it "requests a confirmation email" do
+      visit new_user_confirmation_path
+
+      fill_in 'Email', with: @user.email
+      click_button 'Resend confirmation instructions'
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(new_user_session_path)
+      expect(page).to have_content('If your email address exists in our database, you will receive an email with instructions about how to confirm your account in a few minutes.')
+    end
+
+    it "requests a password reset email" do
+      visit new_user_password_path
+
+      fill_in 'Email', with: @user.email
+      click_button 'Send me instructions'
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(new_user_session_path)
+      expect(page).to have_content('If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes.')
+    end
+
+    it "requests an unlock email" do
+      visit new_user_unlock_path
+
+      fill_in 'Email', with: @user.email
+      click_button 'Resend unlock instructions'
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(new_user_session_path)
+      expect(page).to have_content('If your account exists, you will receive an email with instructions about how to unlock it in a few minutes.')
+    end
+
     it "updates registration" do
       sign_in @user
 
