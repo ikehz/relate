6fd6b7a1e3558f21c3cc2db3d9ed32718f3ce772
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-01-15 18:20:23 -0500

    refactor conversation_helper_spec

--- spec/helpers/conversations_helper_spec.rb
+++ spec/helpers/conversations_helper_spec.rb
@@ -4,31 +4,29 @@ describe ConversationsHelper do
   describe "#truncate_participants" do
     context "when given no participants" do
       it "returns empty" do
-        expect(helper.truncate_participants []).to be_empty
+        p = []
+        expect(helper.truncate_participants p).to be_empty
       end
     end
 
     context "when given one participant" do
       it "returns that participant's name" do
-        c = Participant.make
-        expect(helper.truncate_participants [c]).to eq(c.contact_name)
+        p = [Participant.make]
+        expect(helper.truncate_participants p).to eq(p[0].contact_name)
       end
     end
 
     context "when given a conversation with two participants" do
       it "returns 'name, name'" do
-        c = Participant.make
-        d = Participant.make
-        expect(helper.truncate_participants [c, d]).to eq("#{c.contact_name}, #{d.contact_name}")
+        p = Array.new(2) { Participant.make }
+        expect(helper.truncate_participants p).to eq("#{p[0].contact_name}, #{p[1].contact_name}")
       end
     end
 
     context "when given a conversation with three participants" do
       it "returns 'name, name, ...'" do
-        c = Participant.make
-        d = Participant.make
-        e = Participant.make
-        expect(helper.truncate_participants [c, d, e]).to eq("#{c.contact_name}, #{d.contact_name}, ...")
+        p = Array.new(3) { Participant.make }
+        expect(helper.truncate_participants p).to eq("#{p[0].contact_name}, #{p[1].contact_name}, ...")
       end
     end
   end
